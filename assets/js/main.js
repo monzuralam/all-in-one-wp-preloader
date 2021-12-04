6df80c98ec264403f9e9e4dc4d6973db1aae7039
monzuralam <monzur707@gmail.com>
2021-12-04 18:28:52 +0600

    Essential JS  include

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+(function($){
+    $(window).load(function () {
+        $("body").addClass("page-loaded");
+
+        if ($('.ai1wp-style-3').length) {
+            $('.ai1wp-style-3').fadeOut('slow');
+        }
+    });
+})(jQuery);
